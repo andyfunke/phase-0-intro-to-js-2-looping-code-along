200d43deacefc1fe8d73ade6f381036b428e0327
Andrew <andrew.i.funke@gmail.com>
2023-06-21 12:10:46 -0700

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(stringArray,eventName) {
+    const emptyArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+    emptyArray[i] = (`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return emptyArray;
+
+}
+
+function countDown(number) {
+    let countdown = number;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
