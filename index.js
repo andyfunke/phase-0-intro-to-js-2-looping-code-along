83b2a585f2c28558479539442c60fcb822b354e8
Andrew <andrew.i.funke@gmail.com>
2023-07-05 09:09:40 -0700

    Redo of Looping Lab

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger;
+function writeCards(stringArray, eventName){
+  let newArray = [];  
+  for (let i=0; i < stringArray.length; i++){
+    newArray.push(`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`)
   }
-
-  return gifts;
+  return newArray;
 }
 
-wrapGifts(gifts);
+// function countDown(number){
+//   for (let i=number; i >= 0; i--)
+//   console.log(i);
+// }
 
-function writeCards(stringArray,eventName) {
-    const emptyArray = [];
-    for (let i = 0; i < stringArray.length; i++) {
-    emptyArray[i] = (`Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`);
-    }
-    return emptyArray;
-
-}
-
-function countDown(number) {
-    let countdown = number;
-    while (countdown >= 0) {
-        console.log(countdown--);
-    }
+function countDown(number){
+  let i=number;
+  while (i>=0){
+    console.log(i--);
+  }
 }
